eba953e99252e87d18c27fe48744d19b92befc8c
hossainnirob <shajadanirob@gmail.com>
2024-05-20 22:57:08 +0800

    setup a backend sever

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+// import { StudentRoutes } from './app/modules/Student/student.route';
+
+const app: Application = express();
+
+//parsers
+app.use(express.json());
+app.use(cors());
+
+// application routes
+// app.use('/api/v1/students', StudentRoutes);
+
+const getAController = (req: Request, res: Response) => {
+  res.send('Welcome our server');
+};
+
+app.get('/', getAController);
+
+export default app;
