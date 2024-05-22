5a2fb20f967c3f3849544030cdc90b00fc7a3f5b
hossainnirob <shajadanirob@gmail.com>
2024-05-23 07:40:02 +0800

    relateable cooment in product route

--- src/app/modules/Products/products.route.ts
+++ src/app/modules/Products/products.route.ts
@@ -1,13 +1,13 @@
-
 import express from 'express';
-import { ProductController } from '../products.controller';
-const router = express.Router();
+import { ProductController } from '../products.controller'; // Importing the product controller
 
-router.post('/', ProductController.createProduct);
-router.get('/:productId', ProductController.getSingleProduct);
-router.get('/', ProductController.getAllProduct);
-router.put('/:productId', ProductController.updateProducts);
-router.delete('/:productId', ProductController.deleteProduct);
+const router = express.Router(); // Creating an instance of Express router
 
+// Defining routes and their corresponding controller methods
+router.post('/', ProductController.createProduct); // Route for creating a new product
+router.get('/:productId', ProductController.getSingleProduct); // Route for getting a single product by ID
+router.get('/', ProductController.getAllProduct); // Route for getting all products
+router.put('/:productId', ProductController.updateProducts); // Route for updating a product by ID
+router.delete('/:productId', ProductController.deleteProduct); // Route for deleting a product by ID
 
-export const ProductRoute = router;
+export const ProductRoute = router; // Exporting the router as ProductRoute
